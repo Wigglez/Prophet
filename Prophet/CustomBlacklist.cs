9d19bae484f8597bfdf1757713d41491a1afe18e
Wigglez <lod_damageinc@hotmail.com>
2014-01-05 05:12:34 -0600

    Had unneeded methods

--- Prophet/CustomBlacklist.cs
+++ Prophet/CustomBlacklist.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using Styx.Common.Helpers;
-using Styx.CommonBot.Database;
-using Styx.WoWInternals.WoWObjects;
 
-namespace Chameleon.Helpers {
+namespace Prophet {
     public class CustomBlacklist {
         // ===========================================================
         // Constants
@@ -15,8 +13,6 @@ namespace Chameleon.Helpers {
         // Fields
         // ===========================================================
 
-        private static readonly Dictionary<ulong, DateTime> GuidBlacklist = new Dictionary<ulong, DateTime>();
-        private static readonly Dictionary<int, DateTime> EntryBlacklist = new Dictionary<int, DateTime>();
         private static readonly Dictionary<string, DateTime> NameBlacklist = new Dictionary<string, DateTime>();
 
         private static WaitTimer _sweepTimer;
@@ -38,95 +34,9 @@ namespace Chameleon.Helpers {
         // ===========================================================
 
         public static void SweepTimer() {
-            TimeSpan maxSweepTime = TimeSpan.FromSeconds(30);
+            var maxSweepTime = TimeSpan.FromSeconds(30);
 
             _sweepTimer = new WaitTimer(maxSweepTime) {WaitTime = maxSweepTime};
-        }
-
-        // GUID
-        public static void AddGUID(ulong guid, TimeSpan timeSpan) {
-            RemoveExpiredGUID();
-            GuidBlacklist[guid] = DateTime.Now.Add(timeSpan);
-        }
-
-        public static void AddGUID(WoWObject wowObject, TimeSpan timeSpan) {
-            if(wowObject != null) {
-                AddGUID(wowObject.Guid, timeSpan);
-            }
-        }
-
-
-        public static bool ContainsGUID(ulong guid) {
-            DateTime expiry;
-            if(GuidBlacklist.TryGetValue(guid, out expiry)) {
-                return (expiry > DateTime.Now);
-            }
-            return false;
-        }
-
-        public static bool ContainsGUID(WoWObject wowObject) { return (wowObject != null) && ContainsGUID(wowObject.Guid); }
-
-        public static void RemoveExpiredGUID() {
-            if(_sweepTimer.IsFinished) {
-                DateTime now = DateTime.Now;
-                List<ulong> expiredEntries = (from key in GuidBlacklist.Keys where (GuidBlacklist[key] < now) select key).ToList();
-                foreach(ulong entry in expiredEntries) {
-                    GuidBlacklist.Remove(entry);
-                }
-                _sweepTimer.Reset();
-            }
-        }
-
-        public static void RemoveAllGUID() {
-            List<ulong> everything = (from key in GuidBlacklist.Keys select key).ToList();
-            foreach(ulong entry in everything) {
-                GuidBlacklist.Remove(entry);
-            }
-        }
-
-        // Entry
-        public static void AddEntry(int entry, TimeSpan timeSpan) {
-            RemoveExpiredGUID();
-            EntryBlacklist[entry] = DateTime.Now.Add(timeSpan);
-        }
-
-        public static void AddEntry(NpcResult npcResult, TimeSpan timeSpan) {
-            if(npcResult != null) {
-                AddEntry(npcResult.Entry, timeSpan);
-            }
-        }
-
-        public static bool ContainsEntry(int entry) {
-            DateTime expiry;
-            if(EntryBlacklist.TryGetValue(entry, out expiry)) {
-                return (expiry > DateTime.Now);
-            }
-            return false;
-        }
-
-        public static bool ContainsEntry(NpcResult npcResult) { return (npcResult != null) && ContainsEntry(npcResult.Entry); }
-
-        public static void RemoveExpiredEntry() {
-            if(!_sweepTimer.IsFinished) {
-                return;
-            }
-
-            var now = DateTime.Now;
-            var expiredEntries = (from key in EntryBlacklist.Keys where (EntryBlacklist[key] < now) select key).ToList();
-
-            foreach(var entry in expiredEntries) {
-                EntryBlacklist.Remove(entry);
-            }
-
-            _sweepTimer.Reset();
-        }
-
-        public static void RemoveAllEntries() {
-            var everything = (from key in EntryBlacklist.Keys select key).ToList();
-
-            foreach(var entry in everything) {
-                EntryBlacklist.Remove(entry);
-            }
         }
 
         // Name
